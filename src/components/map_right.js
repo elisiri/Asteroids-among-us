11ccabc5697ef2e8c71c811b939b015b7e6dac0b
elisa <eli.gort@yahoo.it>
2018-06-20 15:56:50 +0200

    labels

--- src/components/map_right.js
+++ src/components/map_right.js
@@ -13,57 +13,66 @@ class MapRight extends Component {
   }
 
   componentDidMount() {
-
+    console.log(this.state.meteorsFiltered)
   }
 
   componentWillMount() {
     this.projection = geoMercator()
     this.pathGenerator = geoPath().projection(this.projection.scale(250).translate([650, 450]))
+    this.setState({meteorsFiltered: _.filter(meteordata, (item) => {
+      return item.geolocation  && item.mass && item.mass < 1000000
+    })})
   }
 
-  expandLine(coord, e) {
+  expandLine(coord, e, item, i) {
+
     var lineY2 = this.projection(coord)[1]
     // console.log(e.target.nextSibling.getAttribute('y2'))
     // var y2 = e.target.nextSibling.getAttribute('y2')
     // e.target.nextSibling.setAttribute('y2', y2 + 90)
 
-    if (e) return lineY2 + 90
-
-    return lineY2
+    return lineY2 + 80
   }
 
   render() {
 
     const countries = worlddata.features.map((item, i) => <path key={'path' + i} d={this.pathGenerator(item)} className='countries'/>)
-    var meteorsFiltered = _.filter(meteordata, (item) => {
-      return item.geolocation  && item.mass && item.mass < 1000000
-    })
-    var maxMass = max(_.map(meteorsFiltered, (item) => {
-      return parseFloat(item.mass)
-    }))
-    var minMass = min(_.map(meteorsFiltered, (item) => {
-      return parseFloat(item.mass)
-    }))
 
+    var maxMass = max(_.map(this.state.meteorsFiltered, (item) => {
+      return parseFloat(item.mass)
+    }))
+    var minMass = min(_.map(this.state.meteorsFiltered, (item) => {
+      return parseFloat(item.mass)
+    }))
     var convertMasses = scaleLinear().domain([minMass, maxMass]).range([2, 20])
-    const meteorites = meteorsFiltered.map((item, i) =>   {
+    const meteorites = this.state.meteorsFiltered.map((item, i) =>   {
 
             return(
-                                                            <svg>
-                                                            <circle key={'patho' + i}
-                                                            onMouseEnter={e => {this.expandLine(item.geolocation.coordinates, e)}}
 
-                                                            cx={this.projection(item.geolocation.coordinates)[0]}
-                                                            cy={this.projection(item.geolocation.coordinates)[1]}
-                                                            r={convertMasses(parseFloat(item.mass))} className='meteors'/>
-                                                          <line key={'pathos' + i}
-                                                            x1={this.projection(item.geolocation.coordinates)[0]}
-                                                            y1={this.projection(item.geolocation.coordinates)[1]}
-                                                            x2={this.projection(item.geolocation.coordinates)[0]}
-                                                            y2={this.expandLine(item.geolocation.coordinates)}
-                                                            stroke="black" strokeWidth="0.6" className="line"/>
+                                                              <svg className="singleSvg">
+                                                                <circle key={'patho' + i}
+                                                                onMouseEnter={e => {this.expandLine(item.geolocation.coordinates, e, item, i)}}
 
+                                                                cx={this.projection(item.geolocation.coordinates)[0]}
+                                                                cy={this.projection(item.geolocation.coordinates)[1]}
+                                                                r={convertMasses(parseFloat(item.mass))} className='meteors'/>
+                                                              <line key={'pathos' + i}
+                                                                x1={this.projection(item.geolocation.coordinates)[0]}
+                                                                y1={this.projection(item.geolocation.coordinates)[1]}
+                                                                x2={this.projection(item.geolocation.coordinates)[0]}
+                                                                y2={this.projection(item.geolocation.coordinates)[1] + 80}
+                                                                stroke="black" strokeWidth="0.6" className="line"/>
+                                                              <rect key={'pa' + i} className="label"
+                                                                x={this.projection(item.geolocation.coordinates)[0] + 8}
+                                                                y={this.projection(item.geolocation.coordinates)[1] + 50}/>
+                                                              <text key={'pat' + i} className="label-text"
+                                                                x={this.projection(item.geolocation.coordinates)[0] + 18}
+                                                                y={this.projection(item.geolocation.coordinates)[1] + 69}>
+                                                                {item.name} </text>
                                                             </svg>
+
+
+
             )})
 
     var clientHeight = window.document.documentElement.clientHeight
